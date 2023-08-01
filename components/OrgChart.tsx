642f9dd5613b4b6d5d2b4a0d96412137fe73c7da
keispace(desk) <keispace.kyj@gmail.com>
2023-08-01 15:35:03 +0900

    add orgChart

---
+++ components/OrgChart.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Tree, TreeNode } from 'react-organizational-chart'
+import styles from './Components.module.scss'
+
+export default () => (
+  <div className={styles.org}>
+    {document ? (
+      <Tree lineWidth={'1px'} lineStyle="dashed" lineColor={'green'} label={<div className={styles.ceo}>CEO</div>}>
+        {/* <div className={styles.line}></div> */}
+        <TreeNode
+          label={
+            <div className={`${styles.lv1wrap}`}>
+              <div className={styles.lv1}>상품 기획팀</div>
+              <div className={styles.desc}>신규 상품 및 브랜드 기획</div>
+              <div className={styles.desc}>유통 채널 운영</div>
+              <div className={styles.desc}>물류 협력사 관리</div>
+            </div>
+          }></TreeNode>
+        <TreeNode
+          label={
+            <div className={styles.lv1wrap}>
+              <div className={styles.lv1}>해외 기획팀</div>
+              <div className={styles.desc}>해외상품 및 브랜드 소싱</div>
+              <div className={styles.desc}>해외 파트너사 제휴</div>
+            </div>
+          }></TreeNode>
+        <TreeNode
+          label={
+            <div className={styles.lv1wrap}>
+              <div className={styles.lv1}>경영 관리팀</div>
+              <div className={styles.desc}>경영지원 업무</div>
+              <div className={styles.desc}>운영 및 관리</div>
+            </div>
+          }></TreeNode>
+        <TreeNode
+          label={
+            <div className={`${styles.lv1wrap} ${styles.last}`}>
+              <div className={styles.lv1}>생산 관리팀</div>
+              <div className={styles.desc}>제품 생산 및 품질 관리</div>
+            </div>
+          }></TreeNode>
+      </Tree>
+    ) : null}
+  </div>
+)
--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,19 +1,22 @@
+// import { ExampleTree } from '@components/OrgChart'
 import style from './About.module.scss'
+import dynamic from 'next/dynamic'
+
+const OrgChart = dynamic(() => import('@components/OrgChart'), { ssr: false })
 export default function About() {
   return (
     <div className="container">
       <main>
-        <img
-          src="/about/bg1.png"
-          style={{ width: '100vw', maxHeight: '700px' }}
-        />
+        <img src="/about/bg1.png" style={{ width: '100vw', maxHeight: '700px' }} />
 
         <div className={style.bg2}>
           <div className={style.texts}>
             <h6 style={{ color: '#43E195' }}>Organizational Chart.</h6>
             <h2>농업회사법인마음 조직도</h2>
           </div>
-          <div className={style.org}>조직도 패널</div>
+          <div className={style.org}>
+            <OrgChart />
+          </div>
         </div>
 
         <div className={style.bg3}>
@@ -32,8 +35,7 @@ export default function About() {
           <div className={style.history}>
             <div className={style.left}></div>
             <div className={style.right}>
-              <img src="/about/bg4-ring.png" /> <mark>2006</mark>{' '}
-              <p>충북 보은 평사농장 사육</p>
+              <img src="/about/bg4-ring.png" /> <mark>2006</mark> <p>충북 보은 평사농장 사육</p>
             </div>
 
             <div className={style.left}>
@@ -43,28 +45,23 @@ export default function About() {
             <div className={style.right}></div>
             <div className={style.left}></div>
             <div className={style.right}>
-              <img src="/about/bg4-ring.png" /> <mark>2012</mark>{' '}
-              <p>하루 생산량 2만2천구 생산</p>
+              <img src="/about/bg4-ring.png" /> <mark>2012</mark> <p>하루 생산량 2만2천구 생산</p>
             </div>
             <div className={style.left}>
-              <img src="/about/bg4-ring.png" /> <mark>2013</mark>{' '}
-              <p>일양약품 독감백신용 납품</p>
+              <img src="/about/bg4-ring.png" /> <mark>2013</mark> <p>일양약품 독감백신용 납품</p>
             </div>
             <div className={style.right}></div>
             <div className={style.left}></div>
             <div className={style.right}>
-              <img src="/about/bg4-ring.png" /> <mark>2015</mark>{' '}
-              <p>평사농장 증설 일 생산량 5만구 생산</p>
+              <img src="/about/bg4-ring.png" /> <mark>2015</mark> <p>평사농장 증설 일 생산량 5만구 생산</p>
             </div>
             <div className={style.left}>
-              <img src="/about/bg4-ring.png" /> <mark>2017</mark>{' '}
-              <p>한국야쿠르트 납품</p>
+              <img src="/about/bg4-ring.png" /> <mark>2017</mark> <p>한국야쿠르트 납품</p>
             </div>
             <div className={style.right}></div>
             <div className={style.left}></div>
             <div className={style.right}>
-              <img src="/about/bg4-ring.png" /> <mark>2023</mark>{' '}
-              <p>유통전문회사 농업법인 출범</p>
+              <img src="/about/bg4-ring.png" /> <mark>2023</mark> <p>유통전문회사 농업법인 출범</p>
             </div>
           </div>
         </div>
@@ -73,10 +70,7 @@ export default function About() {
           <div className={style.texts}>
             <h6 style={{ color: '#43E195' }}>Partners.</h6>
             <h2>파트너사</h2>
-            <h5>
-              농업회사법인마음은 신선하고 안전한 제품을 위해 국내 및 해외
-              파트너들과 함께 뛰고 있습니다.
-            </h5>
+            <h5>농업회사법인마음은 신선하고 안전한 제품을 위해 국내 및 해외 파트너들과 함께 뛰고 있습니다.</h5>
           </div>
           <div className={style.partners}>
             <div>
